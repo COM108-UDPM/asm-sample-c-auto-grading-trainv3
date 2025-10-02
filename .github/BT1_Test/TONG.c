ba30ae76237ab1335653ea9e1cd9aced3bedaebd
trainv3 <nguyenvan.trai@hotmail.com>
2025-10-02 20:38:51 +0700

    T1

---
+++ .github/BT1_Test/TONG.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int a, b, Tong, Hieu;
+int main()
+{
+printf("Nhap so a="); 
+scanf("%d",&a);
+printf("Nhap so b=");
+scanf("%d",&b);
+ Tong=a+b;
+ Hieu=a-b;
+printf("Tong a+b =%d",Tong);
+printf("\n Hieu a-b=%d",Hieu);
+return 0;
+}
+//DA NHAN FILE
